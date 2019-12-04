648ba47e543756d75049f747ce32d74253738081
Previtali_Fabio <fabprev93@gmail.com>
2019-12-04 10:39:10 +0100

    Migliorata logica di ricerca

--- main.js
+++ main.js
@@ -3,36 +3,42 @@ $(document).ready(function() {
         // Sezione della ricezione del messaggio scritto dall'utente.
         var testo = $(".container-right input").val();
         var nuovo = $(".template .message.user").clone();
-        nuovo.children("p:first-child").text(testo);
+        nuovo.children("p.testo").text(testo);
         $(".message-container").append(nuovo);
         $(".container-right input").val("");
         // Sezione del messaggio automatico di risposta del sistema.
         var clock = setTimeout(function(){
             var nuovo = $(".template .message.machine").clone();
-            nuovo.children("p:first-child").text("OK!");
+            nuovo.children("p.testo").text("OK!");
             $(".message-container").append(nuovo);
         }, 1000)
     });
     // Ora proviamo a cambiare icona da microphone a paper-plane con focus sull'elemento.
-    $(".container-right input").focus(function() {
-        $(".container-right i").removeClass("fa-microphone").addClass("fa-paper-plane");
+    $(".container-right .message-box input").focus(function() {
+        $(".container-right .message-box .row-right i").removeClass("fa-microphone").addClass("fa-paper-plane");
     });
-    $(".container-right input").blur(function() {
-        $(".container-right i").removeClass("fa-paper-plane").addClass("fa-microphone");
+    $(".container-right .message-box input").blur(function() {
+        $(".container-right .message-box .row-right i").removeClass("fa-paper-plane").addClass("fa-microphone");
     });
     // Aggiungo funzione di ricerca dei contatti in base alla ricerca nella barra di sinistra.
     $(".container-left .searchbar input").keyup(function() {
         // Seleziono l'input dell'utente e lo uniformo ad uppercase per un confronto corretto (JS è case-sensitive)
         var ricerca = $(".container-left .searchbar input");
         var testo = ricerca.val().toUpperCase();
-        // Vedo di ottenere ogni nome utente:
-        $(".container-left .conversation-container .user").each(function() {
-            // Stiamo ciclando i vari elementi .user: valutiamo se lo strong contiene una parte dell'input.
-            var corrente = $(this).find("strong").text().toUpperCase();
-            if (!corrente.includes(testo)) {
-                $(this).hide();
-            }
-        })
+        // Con questo ciclo if, valutiamo se l'utente sta cancellando il campo di ricerca, nel caso mostro tutto nuovamente.
+        if (testo.length != 0) {
+            // Vedo di ottenere ogni nome utente:
+            $(".container-left .conversation-container .user").each(function() {
+                // Stiamo ciclando i vari elementi .user: valutiamo se lo strong contiene una parte dell'input.
+                var corrente = $(this).find("strong").text().toUpperCase();
+                if (!corrente.includes(testo)) {
+                    $(this).hide();
+                }
+            })
+        }
+        else {
+            $(".container-left .conversation-container .user").show();
+        }
     })
     // Necessario resettare il tutto se usciamo dalla barra di ricerca: uso un blur.
     // (Nel caso volessi mettere la possibilità di swichare tra conversazioni, sopra il blur che segue.)
